30791f9453fbd6369681aeed6704b940d8929e82
Rachit Jain <Rachit24jain@gmail.com>
2024-06-25 17:56:01 +0530

    boiler plate code

---
+++ Boilerplate.java
@@ -0,0 +1,17 @@
+
+// Java is a statically typed language
+// Java is an object-oriented language
+// Java is a platform independent language
+
+// -------------------------------------------------------------------//
+
+
+
+// BoilerPlate Code of Java
+public class Boilerplate{ // classname should be same as the file name 
+    public static void main(String args[]){
+        // main code will be written inside this function
+        System.out.print("Hello World");
+    }
+}
+
