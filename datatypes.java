8e77bfb8893f988abc58cd92779ac2414922e1b9
Rachit Jain <Rachit24jain@gmail.com>
2024-06-26 14:05:51 +0530

    datatypes in java

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes {
+    public static void main(String[] args) {
+        // primitive data types
+        byte b = 10; // size = 1byte [-128 to 127]
+        short s = 20; // size = 2bytes
+        int i = 30;   // size = 4bytes 
+        long l = 40L; // size = 8bytes
+        float f = 50.5f; // size = 4bytes
+        double d = 60.63445; // size = 8bytes 
+        char c = 'A'; // size = 2bytes
+        boolean bool = true; // size = 1byte
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bool);
+    }
+}
