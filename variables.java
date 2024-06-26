6d9d014e5917d6501fef73c2dace035e5d9f3985
Rachit Jain <Rachit24jain@gmail.com>
2024-06-26 14:03:04 +0530

    variables in java

---
+++ variables.java
@@ -0,0 +1,12 @@
+public class variables {
+    public static void main(String args[]){
+        // integer variables
+        int a = 24;
+        int b = 02;
+        int c = 2004;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
