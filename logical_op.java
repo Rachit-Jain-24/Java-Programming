1e092871216ea87a8102a9b33252cb8396974cd1
Rachit Jain <Rachit24jain@gmail.com>
2024-06-28 16:59:29 +0530

    logical operators

--- logical_op.java
+++ logical_op.java
@@ -4,9 +4,9 @@ public class logical_op{
         // &&, ||, !
 
         System.out.println((24>2) && ((24<40)));
-        System.out.println((24<2) && ((24<40)));
-        System.out.println((24==2) || ((24<40)));
-        System.out.println((24!=2) || ((24>40)));
+        System.out.println((1<2) && ((80 < 40)));
+        System.out.println((24==2) || ((24 < 40)));
+        System.out.println((24==2) || ((12654 < 1200)));
 
     }
 }
