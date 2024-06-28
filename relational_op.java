9237742718641662f186336caa61a17703288080
Rachit Jain <Rachit24jain@gmail.com>
2024-06-28 14:58:56 +0530

    Relational operators

---
+++ relational_op.java
@@ -0,0 +1,17 @@
+public class relational_op{
+    public static void main(String[] args){
+        // relational operators:
+        // ==, !=, >, <, >=, <=.
+
+        int a = 23;
+        int b = 24;
+
+        System.out.println((a==b));
+        System.out.println((a!=b));
+        System.out.println((a<b));
+        System.out.println((a>b));
+        System.out.println((a>=b));
+        System.out.println((a<=b));
+
+    }
+}
