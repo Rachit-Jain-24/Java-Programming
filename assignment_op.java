8bca6b99a7767d0f244da2efcd36f64288906b47
Rachit Jain <Rachit24jain@gmail.com>
2024-06-28 17:00:10 +0530

    assignment  operators

---
+++ assignment_op.java
@@ -0,0 +1,15 @@
+public class assignment_op{
+    public static void main(String[] args){
+        //assignment operators
+        // =,+=,-=,*=,/=
+
+        int a = 100;
+        int b = 20;
+
+        System.out.println(a=b);
+        System.out.println(a+=2);
+        System.out.println(b-=10);
+        System.out.println(b*=2);
+        System.out.println(a/=5);
+    }
+}
