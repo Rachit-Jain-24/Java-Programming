4a674778e6e87e38f6b529610bc474795b496cad
Rachit Jain <Rachit24jain@gmail.com>
2024-06-28 14:56:44 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting{
+    public static void main(String[] args){
+        int num = 24;
+        float num1 = (int) num;
+        System.out.println(num);
+        System.out.println(num1);
+        // smaller to larger source conversion is possible in java not larger to smaller
+    }
+}
