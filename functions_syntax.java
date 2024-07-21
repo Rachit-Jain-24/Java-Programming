dd190896684427c08434055cacf3bca15227666b
Rachit jain <rachit24jain@gmail.com>
2024-07-21 16:53:59 +0530

    functions

---
+++ functions_syntax.java
@@ -0,0 +1,15 @@
+public class functions_syntax {
+    
+    public static void firstfn(){
+
+        for(int i=1;i<5;i++){
+            System.out.println("Hello Functionss!");
+        }
+    }
+    
+    
+    public static void main(String args[]){
+
+        firstfn();
+    }
+}
